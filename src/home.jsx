f4c04067f6dfed4feebe43bc773af82b41dcb09a
2legit2fear <nobeltadele.official@gmail.com>
2024-08-11 18:59:23 +0300

    made home page with responsive

---
+++ src/home.jsx
@@ -0,0 +1,121 @@
+import React from 'react';
+import p1 from './assets/MYPIC.jpg'
+import './home.css';
+
+function App() {
+  return (
+    <div>
+      <nav className="navbar navbar-expand-lg navbar-light bg-light">
+        <a className="navbar-brand" href="#">GojoZone</a>
+        <div className="ml-auto">
+          <form className="form-inline">
+            <a href="https://www.google.com" className="search-link">
+              <input
+                className="form-control mr-sm-2"
+                type="search"
+                placeholder="Search"
+                aria-label="Search"
+              />
+              <span className="search-icon">&#128269;</span>
+            </a>
+          </form>
+        </div>
+      </nav>
+
+      <div className="container mt-4">
+        <h1>Home</h1>
+        <div className="row">
+          <Poster
+            imageUrl={p1}
+            title="Sample Title That Exceeds 28 Characters"
+            price="$99.99"
+          />
+          <Poster
+            imageUrl={p1}
+            title="Short Title"
+            price="$49.99"
+          />
+          <Poster
+            imageUrl={p1}
+            title="Short Title"
+            price="$49.99"
+          />
+          <Poster
+            imageUrl={p1}
+            title="Short Title"
+            price="$49.99"
+          />
+          <Poster
+            imageUrl={p1}
+            title="Short Title"
+            price="$49.99"
+          />
+
+          <Poster
+            imageUrl={p1}
+            title="Short Title"
+            price="$49.99"
+          />
+          <Poster
+            imageUrl={p1}
+            title="Short Title"
+            price="$49.99"
+          />
+          <Poster
+            imageUrl={p1}
+            title="Short Title"
+            price="$49.99"
+          />
+          <Poster
+            imageUrl={p1}
+            title="Short Title"
+            price="$49.99"
+          />
+          <Poster
+            imageUrl={p1}
+            title="Short Title"
+            price="$49.99"
+          />
+          <Poster
+            imageUrl={p1}
+            title="Short Title"
+            price="$49.99"
+          />
+          <Poster
+            imageUrl={p1}
+            title="Short Title"
+            price="$49.99"
+          />
+          <Poster
+            imageUrl={p1}
+            title="Short Title"
+            price="$49.99"
+          />
+          
+          {/* Add more <Poster /> components as needed */}
+        </div>
+      </div>
+    </div>
+  );
+}
+
+function Poster({ imageUrl, title, price }) {
+  // Function to truncate title if it exceeds 28 characters
+  const truncateTitle = (str) => {
+    return str.length > 28 ? str.substring(0, 28) + "..." : str;
+  };
+
+  return (
+    <div className="col-md-4 mb-4">
+      <div className="poster">
+        <img src={imageUrl} alt="Poster" className="poster-img" />
+        <div className="poster-content">
+          <h5 className="poster-title">{truncateTitle(title)}</h5>
+          <p className="poster-price">{price}</p>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default App;
